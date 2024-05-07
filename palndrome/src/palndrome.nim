858fae5ca5dcd5d57e21e52ba257bc4e4b747224
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-05-07 19:37:52 +0300

    ok

--- palndrome/src/palndrome.nim
+++ palndrome/src/palndrome.nim
@@ -1,16 +1,25 @@
 echo "Please Enter string"
 
-let word="word"
+let word=readLine(stdin)
 
 var back=word.len()-1
 var front=0
+var state=true
 
 while -1!=back:
   if word[back]!=word[front]:
-    echo "Not Palandrome"
+    state=false
     break
   dec(back,1)
   inc(front,1)
+
+
+if state==false:
+  echo "Not Plandrome"
+else:
+  echo "Plandrome"
+  
+
 
 
   
