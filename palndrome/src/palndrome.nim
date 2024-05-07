9051405212827f769a5b0b8e0c9a8820d360c6ae
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-05-07 19:33:19 +0300

    ok

--- palndrome/src/palndrome.nim
+++ palndrome/src/palndrome.nim
@@ -2,11 +2,13 @@ echo "Please Enter string"
 
 let word="word"
 
-var num=word.len()-1
+var back=word.len()-1
+var front=0
 
-while -1!=num:
-  echo word[num]
-  dec(num,1)
+while -1!=back:
+  echo(word[back],word[front])
+  dec(back,1)
+  inc(front,1)
 
 
   
