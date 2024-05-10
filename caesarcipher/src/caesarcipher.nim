e9383dc4a1dfcd8e708c65eeeaaa5c1b034ede7b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-05-10 20:15:23 +0300

    ok

--- caesarcipher/src/caesarcipher.nim
+++ caesarcipher/src/caesarcipher.nim
@@ -1,7 +1,15 @@
+import std/math
 echo "Enter text to encrypt"
 
 # i choose the number 7 b/c most human pick it b/n 0-10 if told to pick randomly
 const key=7
 
 let word=readLine(stdin)
+
+for index in word:
+  var encrypted:int64=(int)index
+  encrypted=encrypted+key
+  encrypted=encrypted mod 26
+  echo encrypted
+  
 
