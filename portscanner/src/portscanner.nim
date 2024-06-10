b4cc0e59472997876530dda51cd1e25e8d6f5690
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-10 16:17:47 +0300

    ok

--- portscanner/src/portscanner.nim
+++ portscanner/src/portscanner.nim
@@ -2,14 +2,21 @@ import net
 
 proc portScanner(port:int)=
     var client=newSocket()
+    var a=0
     try:
-        client.connect("142.250.187.142",Port(port),80)
+        client.connect("213.55.95.15",Port(port),110)
+        a=1
     except OSError as e:
         discard e
     except TimeoutError as te:
         discard te
     finally:
-        echo port
-    
-portScanner(80)
+        if a==1:
+            echo "open port at: ",port
+        else:
+            echo "at ",port
+
+# ok this works
+for i in 0 .. 65000:
+    portScanner(i)
 
