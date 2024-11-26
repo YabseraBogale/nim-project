4676783df14371ca5e64900e80a7cdf3d8be3126
YabseraBogale <yabserapython@gmail.com>
2024-11-26 16:34:08 +0300

    ok

--- makeFlask/src/makeFlask.nim
+++
@@ -1,8 +0,0 @@
-import httpclient,strutils
-
-let client=newHttpClient()
-
-var res=client.getContent("https://news.ycombinator.com/item?id=8984648")
-
-for i in res.splitLines():
-  echo i.contains("div")
